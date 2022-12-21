e953eea307c8f2fa646c792851467de3e837370e
Quisette <quisette@gmail.com>
2022-12-21 23:29:35 +0800

    add scripts for stopping mongodb and flask backend service

--- pymongo/serve.sh
+++ pymongo/serve.sh
@@ -1,3 +1,19 @@
 sudo mongod  --dbpath ./db --fork --logpath /var/log/mongodb/mongod.log
-source ../venv/bin/activate
-flask --app app.py --debug run
+#source ../venv/bin/activate
+on_exit()
+{
+        echo "exiting mongod service...."
+        mongopid=$(pgrep mongod)
+        echo "the pid for mongod is $mongopid, killing..."
+        {
+                sudo kill "$mongopid"
+                echo "killing process has completed!"
+        }||{
+                echo "error deleting mongod."
+        }
+}
+#trap on_exit EXIT
+nohup flask --app app.py --debug run &
+
+
+
